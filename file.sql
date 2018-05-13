7eb38f0dba869a5240ca01a2b440d68efccf6cd1
easonHua97 <15906609020@163.com>
2018-05-13 22:00:25 +0200

    add sql file

---
+++ file.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA `cookbook_group3` ;
+
+CREATE TABLE `cookbook_group3`.`user` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(20) NOT NULL,
+  `password` VARCHAR(50) NOT NULL,
+  `status` INT NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
+  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC))
+ENGINE = InnoDB;
