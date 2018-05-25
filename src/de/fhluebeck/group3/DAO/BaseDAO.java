f2a151d9567192cd1caecca08cad4a0176bb8938
easonhua <15906609020@163.com>
2018-05-25 13:24:47 +0200

    add delete User to UserDAO

--- src/de/fhluebeck/group3/DAO/BaseDAO.java
+++ src/de/fhluebeck/group3/DAO/BaseDAO.java
@@ -3,17 +3,22 @@ package de.fhluebeck.group3.DAO;
 import java.sql.*;
 
 /**
- * BaseDAO as the Factory Model. This class is mainly responsible for MYSQL database connection,
- * which sets the connection parameters, load the driver at the beginning of the system and provides 
- * functions for acquiring and closing DB connections.  
+ * BaseDAO as the Factory Model. This class is mainly responsible for MYSQL
+ * database connection, which sets the connection parameters, load the driver at
+ * the beginning of the system and provides functions for acquiring and closing
+ * DB connections.
  * 
  * @author Yichen.Hua on 2018/05/22.
  */
 public final class BaseDAO {
-
+	
+	private static Connection conn = null;
+	private static PreparedStatement pstmt = null;
+	private static ResultSet rs = null;
+	
 	/**
 	 * Basic attributes for database.
-	 * */
+	 */
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://localhost:3306/cookbook_group3?characterEncoding=utf-8";
 	private static final String USERNAME = "root";
@@ -21,7 +26,7 @@ public final class BaseDAO {
 
 	/**
 	 * Load the driver for database connection at the beginning of the system.
-	 * */
+	 */
 	static {
 		try {
 			Class.forName(DRIVER);
@@ -34,63 +39,154 @@ public final class BaseDAO {
 	 * Provider the external classes with the valid DB connections.
 	 * 
 	 * @return connection valid database connection.
-	 * @throws SQLException when connection is not available.
-	 * */
+	 * @throws SQLException
+	 *             when connection is not available.
+	 */
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(URL, USERNAME, PASSWORD);
 	}
 
 	/**
-	 * Provider the external classes with the close function of DB connections resources.
+	 * Provider the external classes with the close function of DB connections
+	 * resources.
 	 * 
-	 * @param conn DB Connection.
-	 * @param stmt Statement.
-	 * @param rs ResultSet. 
+	 * @param conn
+	 *            DB Connection.
+	 * @param stmt
+	 *            Statement.
+	 * @param rs
+	 *            ResultSet.
 	 * 
-	 * @throws SQLException when SQLException occurs.
-	 * */
+	 * @throws SQLException
+	 *             when SQLException occurs.
+	 */
 	public static void closeAll(Connection conn, Statement stmt, ResultSet rs) throws SQLException {
-		if (rs != null) {
+		if (rs != null && !rs.isClosed()) {
 			rs.close();
 		}
-		if (stmt != null) {
+		if (stmt != null && !stmt.isClosed()) {
 			stmt.close();
 		}
-		if (conn != null) {
+		if (conn != null && !conn.isClosed()) {
 			conn.close();
 		}
 	}
-	
+
 	/**
-	 * Fill in the wild-cards with attribute in the Object[] and Execute SQL query, returns the result set.
+	 * Fill in the wild-cards with attribute in the Object[] and Execute SQL query,
+	 * returns the result set.
+	 * Note that you do have to close the resources by calling our closeAll function.
 	 * 
-	 * @param preparedSql: raw SQL statements with wild-cards.
-	 * @param param: parameters to fill in the wild-cards.
+	 * @param preparedSql:
+	 *            raw SQL statements with wild-cards.
+	 * @param param:
+	 *            parameters to fill in the wild-cards.
 	 * 
 	 * @return ResultSet: SQL querying results.
 	 * 
-	 * @throws ClassNotFoundException: when the driver class is not found.
-	 * */
-	public static ResultSet executeSQL(String preparedSql, Object[] param) throws ClassNotFoundException {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        /* deal with and execute SQL */
-        try {
-            conn = getConnection(); // get database link from this class
-            pstmt = conn.prepareStatement(preparedSql); // get PreparedStatement object
-            if (param != null) {
-                for (int i = 0; i < param.length; i++) {
-                    pstmt.setObject(i + 1, param[i]); // set parameters for prepared statement
-                }
-            }
-        rs = pstmt.executeQuery(); // execute the SQL expression
-        } catch (SQLException e) {
-            e.printStackTrace(); // handle SQLException
-        } 
-        
-        return rs;
-    }
-	
+	 * @throws ClassNotFoundException:
+	 *             when the driver class is not found.
+	 */
+	public static ResultSet executeQuery(String preparedSql, Object[] param) throws ClassNotFoundException {
+		/* deal with and execute SQL */
+		try {
+			conn = getConnection(); // get database link from this class
+			pstmt = conn.prepareStatement(preparedSql); // get PreparedStatement object
+			if (param != null) {
+				for (int i = 0; i < param.length; i++) {
+					pstmt.setObject(i + 1, param[i]); // set parameters for prepared statement
+				}
+			}
+			rs = pstmt.executeQuery(); // execute the SQL expression
+		} catch (SQLException e) {
+			e.printStackTrace(); // handle SQLException
+		} 
+		return rs;
+	}
+
+	/**
+	 * Fill in the wild-cards with attribute in the Object[] and Execute SQL query,
+	 * returns a boolean attribute. 
+	 * Note that you do not have to close the resources since they are automatically closed by us.
+	 * 
+	 * @param preparedSql:
+	 *            raw SQL statements with wild-cards.
+	 * @param param:
+	 *            parameters to fill in the wild-cards.
+	 * 
+	 * @return boolean - flag that shows whether sql is executed successfully.
+	 * 
+	 * @throws ClassNotFoundException:
+	 *             when the driver class is not found.
+	 */
+	public static boolean executeSql(String preparedSql, Object[] param) throws ClassNotFoundException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean flag = false;
+		/* deal with and execute SQL */
+		try {
+			conn = getConnection(); // get database link from this class
+			pstmt = conn.prepareStatement(preparedSql); // get PreparedStatement object
+			if (param != null) {
+				for (int i = 0; i < param.length; i++) {
+					pstmt.setObject(i + 1, param[i]); // set parameters for prepared statement
+				}
+			}
+			flag = pstmt.execute(); // execute the SQL expression
+		} catch (SQLException e) {
+			flag = false;
+			e.printStackTrace(); // handle SQLException
+		} finally { // finally close and release resources.
+			try {
+				BaseDAO.closeAll(conn, pstmt, null);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return flag;
+	}
+
+	/**
+	 * @return the conn
+	 */
+	public static Connection getConn() {
+		return conn;
+	}
+
+	/**
+	 * @param conn the conn to set
+	 */
+	public static void setConn(Connection conn) {
+		BaseDAO.conn = conn;
+	}
+
+	/**
+	 * @return the pstmt
+	 */
+	public static PreparedStatement getPstmt() {
+		return pstmt;
+	}
+
+	/**
+	 * @param pstmt the pstmt to set
+	 */
+	public static void setPstmt(PreparedStatement pstmt) {
+		BaseDAO.pstmt = pstmt;
+	}
+
+	/**
+	 * @return the rs
+	 */
+	public static ResultSet getRs() {
+		return rs;
+	}
+
+	/**
+	 * @param rs the rs to set
+	 */
+	public static void setRs(ResultSet rs) {
+		BaseDAO.rs = rs;
+	}
 
 }
--- src/de/fhluebeck/group3/DAO/UserDAO.java
+++ src/de/fhluebeck/group3/DAO/UserDAO.java
@@ -12,53 +12,79 @@ import de.fhluebeck.group3.model.User;
  */
 public final class UserDAO {
 
-	private static Connection conn = null;
-	private static PreparedStatement pstmt = null;
-	private static ResultSet rs = null;
+//	private static Connection conn = null;
+//	private static PreparedStatement pstmt = null;
+//	private static ResultSet rs = null;
 
 	/**
 	 * Validate the input credentials and return user object if exists.
 	 * 
-	 * @param username user name
-	 * @param password password
+	 * @param username
+	 *            user name
+	 * @param password
+	 *            password
 	 * 
 	 * @return User: entity object if user exists.
-	 * */
+	 */
 	public static User validatePassword(String username, String password) {
 		User user = null;
+		ResultSet resultSet = null;
 		try {
-			String preparedSql = "SELECT * from user where username = ? and password = ?";
+			String preparedSql = "SELECT * FROM user WHERE username = ? and password = ?";
 			Object[] parameters = { username, password };
-			UserDAO.rs = BaseDAO.executeSQL(preparedSql, parameters);
-			if (UserDAO.rs != null) {
-				while (UserDAO.rs.next()) {
+			resultSet = BaseDAO.executeQuery(preparedSql, parameters);
+			if (resultSet != null) {
+				while (resultSet.next()) {
 					user = new User();
-					user.setUserId((Integer.getInteger(UserDAO.rs.getString("user_id"))));
-					user.setUsername(UserDAO.rs.getString("username"));
-					user.setPassword(UserDAO.rs.getString("password"));
-					user.setUserId(Integer.getInteger(UserDAO.rs.getString("status")));
+					user.setUserId((Integer.getInteger(resultSet.getString("user_id"))));
+					user.setUsername(resultSet.getString("username"));
+					user.setPassword(resultSet.getString("password"));
+					user.setUserId(Integer.getInteger(resultSet.getString("status")));
 				}
 			}
-			//TODO also we have to fill its favorite recipes and ownRecipes Lists to make a full user.
+			// TODO also we have to fill its favorite recipes and ownRecipes Lists to make a full user.
 			// which will involve code in RecipeDAO.
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {	//finally close and release resources.
-            try {
-            	BaseDAO.closeAll(UserDAO.conn, UserDAO.pstmt, UserDAO.rs);
-            } catch (SQLException e) {    
-                e.printStackTrace();
-            }
-        }
+		} finally { // finally close and release resources.
+			try {
+				BaseDAO.closeAll(BaseDAO.getConn(),BaseDAO.getPstmt(),BaseDAO.getRs());
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		return user;
 	}
 
 	/**
+	 * Validate the input credentials and return user object if exists.
+	 * 
+	 * @param username
+	 *            user name
+	 * @param password
+	 *            password
+	 * 
+	 * @return User: entity object if user exists.
+	 */
+	public static void deleteUser(Integer userId) {
+		try {
+			String preparedSql = "UPDATE `user` SET `status` = 0 WHERE `user_id` = ?";
+			Object[] parameters = { userId };
+			BaseDAO.executeSql(preparedSql, parameters);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 
+	}
+
+	/*
 	 * Unit test
-	 * */
+	 */
 	public static void main(String[] args) {
-		User user = UserDAO.validatePassword("admin", "8106417f482b5b3a30a433f4a31704bf");
-		System.out.println(user);
+
+		 User user = UserDAO.validatePassword("admin","8106417f482b5b3a30a433f4a31704bf");
+		 System.out.println(user);	//success
+
+//		deleteUser(2); //success
 	}
 
 }
