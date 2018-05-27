dff0bde157b796257f3f0cf476bfeba72bfa365b
komgyu <komgyu@192.168.0.114>
2018-05-27 14:33:33 +0200

    altered by kongyu

--- src/de/fhluebeck/group3/DAO/StepDAO.java
+++ src/de/fhluebeck/group3/DAO/StepDAO.java
@@ -4,7 +4,7 @@ package de.fhluebeck.group3.DAO;
  * StepDAO is major responsible for Data Access in Step table, functions like
  * findStepByRecipeID, updateStep and deleteStep is provided.
  * 
- *	Kongyu
+ *	@author komgyu on 2018/5/27.
  */
 public class StepDAO {
 
