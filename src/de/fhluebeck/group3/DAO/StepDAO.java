1ec0b74a6580ffea55d5b28415da8c7780f21d7f
easonHua97 <15906609020@163.com>
2018-06-03 23:52:08 +0200

    alter the stepDAO

--- src/de/fhluebeck/group3/DAO/StepDAO.java
+++ src/de/fhluebeck/group3/DAO/StepDAO.java
@@ -1,14 +1,14 @@
 package de.fhluebeck.group3.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import de.fhluebeck.group3.model.Recipe;
+
 import de.fhluebeck.group3.model.Step;
-import de.fhluebeck.group3.model.User;
-import de.fhluebeck.group3.util.EncryptUtil;
 
 /**
  * StepDAO is major responsible for Data Access in Step table, functions like
@@ -54,53 +54,55 @@ public final class StepDAO {
 	 * 
 	 * @return flag: whether the function is succeeded or not.
 	 */
-	
-	public static List<Step> searchStepByRecipeId(Integer recipeId) {	
-		Step step = null ;
+
+	public static List<Step> searchStepByRecipeId(Integer recipeId) {
+		Step step = null;
+		Connection connection = null;
+		PreparedStatement pstmt = null;
 		ResultSet resultSet = null;
-		int stepOrder = 0;
-		String content = null;
 		List<Step> steps = new ArrayList<Step>();
-		
-		if(recipeId!=null) {
+
+		if (recipeId == null) {	//if null, return null;
 			return null;
 		}
-		
-		try {
-			//TODO create String preparedSql and parameters. see UserDAO.
-			String preparedSql="SELECT * FROM recipe WHERE recipeId = ? AND status = 1";
-			Object[]parameters={stepOrder, content}
-;			resultSet = BaseDAO.executeQuery(preparedSql,parameters);
 
-			//TODO call the function in BaseDAO, executeQuery; see how I did it in UserDAO.
-			if (resultSet!= null && resultSet.isBeforeFirst()) { // ensure that there are some data in result set.
-				while(resultSet.next()){
-				step = new Step();
-				step.setStepOrder(resultSet.getInt("stepOrder"));
-				step.setContent(resultSet.getString("content"));
-				steps.add(step);
-				System.out.println(resultSet.getString("content"));
-				
+		try {
+			connection = BaseDAO.getConnection();
+			String preparedSql = "SELECT * FROM step WHERE recipeID = ? AND status = 1";
+			pstmt = connection.prepareStatement(preparedSql);
+			Object[] parameters = { recipeId };
+			if (parameters != null && parameters.length > 0) {
+				for (int i = 0; i < parameters.length; i++) {
+					pstmt.setObject(i + 1, parameters[i]); // set parameters for prepared statement
+				}
+			}
+			resultSet = pstmt.executeQuery(); // execute the SQL expression
+
+			if (resultSet != null && resultSet.isBeforeFirst()) { // ensure that there are some data in result set.
+				while (resultSet.next()) {
+					step = new Step();
+					step.setStepOrder(resultSet.getInt("stepOrder"));
+					step.setContent(resultSet.getString("content"));
+					step.setStepID(resultSet.getInt("id"));
+					step.setStatus(resultSet.getInt("status"));
+					step.setRecipeID(resultSet.getInt("recipeID"));
+					steps.add(step);
 				}
 			} else {
-				//TODO if not the result set is empty, print "sorry step not found", and return null;
 				System.out.println("sorry, step not found");
 				return null;
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
-		
-		// TODO Finally, close all the resources, see BaseDAO and UserDAO.
-		finally { // finally close and release resources.
+		}finally { // finally close and release resources.
 			try {
-				BaseDAO.closeAll(BaseDAO.getConn(), BaseDAO.getPstmt(), BaseDAO.getRs());
+				BaseDAO.closeAll(connection,pstmt,resultSet);
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return steps;
 	}
 
@@ -117,28 +119,28 @@ public final class StepDAO {
 
 		return flag;
 	}
-	
+
 	/**
 	 * Unit test for StepDAO.
 	 * 
-	 * @param args: string from console input.
+	 * @param args:
+	 *            string from console input.
 	 */
 	public static void main(String[] args) {
-		
-		List<Step> steptest1 =searchStepByRecipeId(1);
-		
-		//should print "sorry step not found"
-	    List<Step> steptest2= searchStepByRecipeId(100);
-	
-		
+
+		List<Step> steptest1 = searchStepByRecipeId(1);
+
+		// should print "sorry step not found"
+//		List<Step> steptest2 = searchStepByRecipeId(100);
+
 		/**
-		 * print basic information of step, you can set, in the database, some step's status as 0, 
-		 * to test if they will be printed out.
-		 * */
-		
-	    for(Step step:steptest1) {
+		 * print basic information of step, you can set, in the database, some step's
+		 * status as 0, to test if they will be printed out.
+		 */
+
+		for (Step step : steptest1) {
 			System.out.println(step);
-			 
+
 		}
 	}
 
