ae7ab6b4116dd9c4a4f596a9d5c770bcdc2a0272
easonhua <15906609020@163.com>
2018-06-06 08:31:41 +0200

    commit mainframe

--- src/de/fhluebeck/group3/controller/MainFrameController.java
+++ src/de/fhluebeck/group3/controller/MainFrameController.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
