0a62a1ed5e2fd0fe40ac8392f0fa81c8a40c941e
Administrator <Administrator@windows10.microdone.cn>
2018-06-09 15:25:03 +0200

    234

--- src/de/fhluebeck/group3/controller/AddOrEditRecipeController.java
+++ src/de/fhluebeck/group3/controller/AddOrEditRecipeController.java
@@ -1,9 +1,38 @@
 package de.fhluebeck.group3.controller;
 
+import java.awt.Button;
+import java.awt.Image;
+import java.awt.TextArea;
+
+import com.itextpdf.text.pdf.TextField;
+import com.sun.org.apache.xml.internal.security.keys.keyresolver.implementations.PrivateKeyResolver;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+
 /**
  * 
  * @author Eason.Hua on 2018/06/05.
  */
-public final class AddOrEditRecipeController {
-
+public final class AddOrEditRecipeController 
+{
+	@FXML
+	private TextArea nameofRecipe;
+	private TextField Description;
+	private TextArea amountOfPeople;
+	private TextArea preparingTime;
+	private TextArea cookingTime;
+	private Button choosePicture;
+	private Button addIngredient;
+	private Button removeIngredient;
+	private Button removeStep;
+	private Button addStep;
+	private Button save;
+	private TableView ingredients;
+	private TableView steps;
+	private ImageView newRecipeImage;
+	
+	
+	
 }
